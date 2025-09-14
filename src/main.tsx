c5e008d9963c9a3f27aecb968c7b1a8a4d7e8c6d
Naveen <itsnaveenkumar98@gmail.com>
2025-09-14 22:53:37 +0100

    fix for redirection

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+// Get redirect param if coming from 404.html
+const redirect = new URLSearchParams(window.location.search).get("redirect");
+if (redirect) {
+  window.history.replaceState(null, "", "/React-big-calendar" + redirect);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename="/React-big-calendar">
