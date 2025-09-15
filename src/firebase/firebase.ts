04887f55ea3057a4d99eba73aa03073ca4e79949
Naveen <itsnaveenkumar98@gmail.com>
2025-09-15 14:00:04 +0530

    Firebase Config Update

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -7,15 +7,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXGUhz5qTkNWZDLmeSpTZyTg4qI30M28s",
-  authDomain: "authentication-abeb9.firebaseapp.com",
-  projectId: "authentication-abeb9",
-  storageBucket: "authentication-abeb9.firebasestorage.app",
-  messagingSenderId: "897221888572",
-  appId: "1:897221888572:web:f96e26409cdb1b2ddc1139",
-  measurementId: "G-MR0X76Z8DN"
+  apiKey: "AIzaSyDjmsXFDKByU5hodYzwrxmV6bgp0PW-CWk",
+  authDomain: "authentication-fe7ec.firebaseapp.com",
+  projectId: "authentication-fe7ec",
+  storageBucket: "authentication-fe7ec.firebasestorage.app",
+  messagingSenderId: "387106670583",
+  appId: "1:387106670583:web:f52833d1deb566fd3350f2",
+  measurementId: "G-610Z6GGHMN"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
